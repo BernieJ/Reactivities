c304c0da508f4e16423966d26d643c717f58ba66
Bernie Jordaan <bjordaan@singular.co.za>
2022-12-02 11:36:16 +0200

    Section 4
    Api work for section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API.Extensions
+{
+  public static class ApplicationServiceExtensions
+  {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config, string corsPolicy)
+    {
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+      });
+
+      services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseSqlite(config.GetConnectionString("DEfaultConnection"));
+      });
+
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy(corsPolicy, policy =>
+        {
+          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+        });
+      });
+
+      services.AddMediatR(typeof(List.Handler).Assembly);
+      services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+      return services;
+    }
+  }
+}
---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+  public class Delete
+  {
+    public class Command : IRequest
+    {
+      public Guid Id { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+      private readonly DataContext _dataContext;
+      private readonly IMapper _mapper;
+
+      public Handler(DataContext dataContext, IMapper mapper)
+      {
+        _dataContext = dataContext;
+        _mapper = mapper;
+      }
+
+      public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+      {
+        var activity = await _dataContext.Activities.FindAsync(request.Id);
+
+        _dataContext.Activities.Remove(activity);
+        await _dataContext.SaveChangesAsync();
+
+        return Unit.Value;
+      }
+    }
+  }
+}
