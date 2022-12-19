86cbf21ea16561c725c031401792bc729ad6cea4
Bernie Jordaan <bjordaan@singular.co.za>
2022-12-19 21:40:26 +0200

    section 15
    end of section 15

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,7 +12,7 @@ import MyTextArea from '../../../app/common/form/MyTextArea'
 import { categoryOptions } from '../../../app/common/options/categoryOptions'
 import MySelectInput from '../../../app/common/form/MySelectInput'
 import MyDateInput from '../../../app/common/form/MyDateInput'
-import { Activity } from '../../../app/models/activity'
+import { ActivityFormValues } from '../../../app/models/activity'
 
 const ActivityForm = observer(() => {
   const { activityStore } = useStore()
@@ -22,21 +22,14 @@ const ActivityForm = observer(() => {
     createActivity,
     updateActivity,
     loadActivity,
-    loading,
     loadingInitial,
   } = activityStore
 
   const { id } = useParams<{ id: string }>()
 
-  const [activity, setActivity] = useState<Activity>({
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    date: null,
-    city: '',
-    venue: '',
-  })
+  const [activity, setActivity] = useState<ActivityFormValues>(
+    new ActivityFormValues(),
+  )
 
   const validationSchema = Yup.object({
     title: Yup.string().required('The activity title is required'),
@@ -49,12 +42,14 @@ const ActivityForm = observer(() => {
 
   useEffect(() => {
     if (id) {
-      loadActivity(id).then((activity) => setActivity(activity!))
+      loadActivity(id).then((activity) =>
+        setActivity(new ActivityFormValues(activity!)),
+      )
     }
   }, [id, loadActivity])
 
-  function handleFormSubmit(activity: Activity) {
-    if (activity.id.length === 0) {
+  function handleFormSubmit(activity: ActivityFormValues) {
+    if (!activity.id) {
       let newActivity = {
         ...activity,
         id: uuid(),
@@ -105,7 +100,7 @@ const ActivityForm = observer(() => {
             <MyTextInput placeholder="Venue" name="venue"></MyTextInput>
             <Button
               disabled={isSubmitting || !dirty || !isValid}
-              loading={loading}
+              loading={isSubmitting}
               floated="right"
               positive
               type="submit"
